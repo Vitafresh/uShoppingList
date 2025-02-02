6d09a6423745df3627a9a1f618406e1ca06595dc
Vitafresh <vvitafresh@gmail.com>
2025-02-03 00:10:21 +0200

    Shopping List start project

--- app/src/main/java/top/vitafresh/ushoppinglist/MainActivity.kt
+++ app/src/main/java/top/vitafresh/ushoppinglist/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import top.vitafresh.ushoppinglist.ui.theme.UShoppingListTheme
@@ -20,10 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             UShoppingListTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    ShoppingList()
                 }
             }
         }
@@ -31,17 +32,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ShoppingList(){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = { /* TO DO */ },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text(text = "Add Item")
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    UShoppingListTheme {
-        Greeting("Android")
-    }
+fun ShoppingListPreview(){
+    ShoppingList()
 }
